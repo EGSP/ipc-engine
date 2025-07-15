3a93237a95ad7b69506dd2320e13d57d297fb20a
EGSP <repsbro@gmail.com>
2025-07-15 16:00:51 +0300

    123

--- code/server.js
+++ code/server.js
@@ -1,32 +1,31 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const winston = require('winston');
-const yandexClient = require('./services/yandexClient');
-const configService = require('./services/configService');
+import express, { json } from 'express';
+import { createLogger, format as _format, transports as _transports } from 'winston';
+import { initialize } from './services/yandexClient';
+import { config_service } from './services/config_service';
 
+let config = config_service.get_config();
 
 // Инициализация логгера с уровнем из конфига
-const logLevel = configService.getConfigValue('logging.logLevel') || 'info';
+const logLevel = getConfigValue('logging.logLevel') || 'info';
 
-const logger = winston.createLogger({
+const logger = createLogger({
     level: logLevel,
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.printf(({ timestamp, level, message }) => {
+    format: _format.combine(
+        _format.timestamp(),
+        _format.printf(({ timestamp, level, message }) => {
             return `${timestamp} [${level.toUpperCase()}] ${message}`;
         })
     ),
     transports: [
-        new winston.transports.File({ filename: 'logs/app.log' }),
-        new winston.transports.Console()
+        new _transports.File({ filename: 'logs/app.log' }),
+        new _transports.Console()
     ]
 });
 
 // Инициализация Yandex SDK
 try {
-    const iamToken = configService.getConfigValue('yandexCloud.iamToken');
-    yandexClient.initialize(iamToken);
+    const iamToken = getConfigValue('yandexCloud.iamToken');
+    initialize(iamToken);
     logger.info('Yandex SDK инициализирован успешно');
 } catch (error) {
     logger.error(`Ошибка инициализации Yandex SDK: ${error.message}`);
@@ -35,9 +34,9 @@ try {
 
 // Запуск Express
 const app = express();
-const port = configService.getConfigValue('httpServer.port') || 3000;
+const port = getConfigValue('httpServer.port') || 3000;
 
-app.use(express.json());
+app.use(json());
 
 // Заглушка маршрута для теста
 app.get('/ping', (req, res) => {
--- code/services/yandex_client.js
+++ code/services/yandex_client.js
@@ -1,88 +1,87 @@
-import { JWK, JWS } from 'node-jose';
 import {
-    serviceClients,
-    Session,
     cloudApi,
-    waitForOperation,
-    decodeMessage
+    serviceClients,
+    Session
 } from '@yandex-cloud/nodejs-sdk';
-import config_service from './configService';
+import { JWK, JWS } from 'node-jose';
+import config_service from './config_service.js';
 
-let sessionInstance = null;
+let IAM_TOKEN = null;
 
-async function initialize(iamToken) {
-    let config = config_service.get_config();
-    let yandex_config = config_service.get_yandex_config();
+async function get_iam_token() {
+    // TODO: Добавить проверку на уже существующий токен и его время жизни
+    let iam_token = await create_iam();
+    IAM_TOKEN = iam_token;
+    return iam_token;
+}
 
-    async function create_iam() {
-        const {
-            iam: {
-                iam_token_service: { CreateIamTokenRequest }
+/**
+ * Asynchronously creates an IAM token for Yandex Cloud using the provided service account credentials.
+ * 
+ * This function retrieves authentication keys, constructs a session with the Yandex Cloud SDK, 
+ * and uses the IAM Token Service to generate an IAM token. It internally uses a JWT for authentication.
+ * 
+ * @returns {Promise<string>} The generated IAM token.
+ * 
+ * @throws {Error} If there is an issue with creating the JWT or the IAM token.
+ */
+async function create_iam() {
+    const {
+        iam: {
+            iam_token_service: { CreateIamTokenRequest }
+        }
+    } = cloudApi;
+
+    return createIamToken();
+
+    async function createIamToken() {
+        function get_auth_key() {
+            let yandex_config = config_service.get_yandex_config();
+            return yandex_config.auth_key;
+        }
+
+
+        let auth_key = get_auth_key();
+        let {
+            id: access_key_id,
+            service_account_id,
+            private_key
+        } = auth_key;
+
+        const session = new Session({
+            serviceAccountJson: {
+                accessKeyId: access_key_id,
+                serviceAccountId: service_account_id,
+                privateKey: private_key,
             }
-        } = cloudApi;
+        })
+        const tokenClient = session.client(serviceClients.IamTokenServiceClient)
+        const jwt = await createJWT()
+        // @ts-ignore
+        const tokenRequest = CreateIamTokenRequest.fromPartial({ jwt })
+        const { iamToken } = await tokenClient.create(tokenRequest)
 
-        createIamToken();
+        console.log("Получен IAM токен:" + iamToken)
 
-        async function createIamToken() {
+        return iamToken
 
-            let auth_key = get_auth_key();
-            let {
-                id: access_key_id,
-                service_account_id: service_account_id,
-                private_key: private_key
-            } = auth_key;
+        async function createJWT() {
+            const now = Math.floor(new Date().getTime() / 1000);
+            const payload = {
+                iss: service_account_id,
+                iat: now,
+                exp: now + 3600,
+                aud: 'https://iam.api.cloud.yandex.net/iam/v1/tokens'
+            };
 
-            const session = new Session({
-                serviceAccountJson: {
-                    accessKeyId: access_key_id,
-                    service_account_id,
-                    privateKey: private_key,
-                }
-            })
-            const tokenClient = session.client(serviceClients.IamTokenServiceClient)
-            const jwt = await createJWT()
-            const tokenRequest = CreateIamTokenRequest.fromPartial({ jwt })
-            const { iamToken } = await tokenClient.create(tokenRequest)
-
-            console.log("Your iam token:")
-            console.log(iamToken)
-
-            return iamToken
-
-            function createJWT() {
-                const now = Math.floor(new Date().getTime() / 1000);
-                const payload = {
-                    iss: service_account_id,
-                    iat: now,
-                    exp: now + 3600,
-                    aud: 'https://iam.api.cloud.yandex.net/iam/v1/tokens'
-                };
-
-                return JWK.asKey(private_key, 'pem', { kid: access_key_id, alg: 'PS256' }).then(
-                    function (result) {
-                        return JWS.createSign({ format: 'compact' }, result)
-                            .update(JSON.stringify(payload))
-                            .final();
-                    }
-                );
-            }
+            const result_2 = await JWK.asKey(private_key, 'pem', { kid: access_key_id, alg: 'PS256' });
+            return await JWS.createSign({ format: 'compact' }, result_2)
+                .update(JSON.stringify(payload))
+                .final();
         }
     }
 }
 
-function get_auth_key() {
-    let yandex_config = config_service.get_yandex_config();
-    return yandex_config.auth_key;
-}
-
-function getSession() {
-    if (!sessionInstance) {
-        throw new Error('Yandex SDK не инициализирован. Вызовите initialize()');
-    }
-    return sessionInstance;
-}
-
 export default {
-    initialize,
-    getSession
+    get_iam_token
 };
